a00b5eea3953f6a3ee8ce31ae4f1b44e28261a2b
Eric Karschner <erickarschner@gmail.com>
2020-12-03 18:45:43 -0500

    Add alternative way to display Word documents which separates items into cards

--- tabs/hubScreens/MoHScreen.js
+++ tabs/hubScreens/MoHScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import WordDocument from "../../components/WordDocument";
+import WordDocumentCards from "../../components/WordDocumentCards";
 import MoHData from "../../assets/word_documents/MoH";
 
 export default function MoHDocument() {
-  return <WordDocument page={MoHData} />;
+  return <WordDocumentCards page={MoHData} />;
 }
 
 MoHDocument.propTypes = {
