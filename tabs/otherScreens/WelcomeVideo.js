b8ac644c3bce2f22a6f3daf70d30b48897662d20
devyboy <dev@udel.edu>
2021-03-01 08:30:49 -0500

    Revert "test"
    This reverts commit c4235c8cbc42d620fe1328ec3b3cdd5b1d77771e.

--- tabs/otherScreens/WelcomeVideo.js
+++ tabs/otherScreens/WelcomeVideo.js
@@ -1,36 +1,41 @@
-import * as React from "react";
-import { View, StyleSheet, Button, Text } from "react-native";
-import { Video } from "expo-av";
+import * as React from 'react';
+import { View, StyleSheet, Button, Text } from 'react-native';
+import { Video } from 'expo-av';
 
-export default function WelcomeVideo({ navigation }) {
+export default function WelcomeVideo({navigation}) {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
-  const [didWatch, setDidWatch] = React.useState(false);
+    const [didWatch, setDidWatch] = React.useState(false)
   React.useEffect(() => {
-    if (status.didJustFinish) {
-      setDidWatch(true);
+    if(status.didJustFinish) {
+        setDidWatch(true)
     }
-  }, [status]);
+
+  }, [status])
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome!</Text>
+        <Text style={styles.text}>Welcome!</Text>
       <Video
         ref={video}
         style={styles.video}
         source={{
-          uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
         }}
-        resizeMode="cover"
-        useNativeControls
-        onPlaybackStatusUpdate={(status) => setStatus(status)}
+        resizeMode='stretch'
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
       />
       <View style={styles.buttons}>
-        {didWatch && (
-          <Button
-            onPress={() => navigation.navigate("Check List")}
-            title={"Continue ->"}
-          />
-        )}
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+        <Button title="Go full screen" onPress={() => {
+            video.current.presentFullscreenPlayer()
+        }}/>
+        {didWatch && <Button onPress={() => navigation.navigate('Check List')}title={'Continue ->'}></Button>}
       </View>
     </View>
   );
@@ -39,20 +44,20 @@ export default function WelcomeVideo({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
   text: {
-    textAlign: "center",
+      textAlign: 'center'
   },
   video: {
-    alignSelf: "center",
+    alignSelf: 'center',
     width: 400,
     height: 250,
   },
   buttons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
