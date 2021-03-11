72834e815d9ed35ac8cd638a93127986b666c843
Mike <mhwilkes@gmail.com>
2021-03-11 12:17:56 -0500

    remove require to ES6 style

--- App.js
+++ App.js
@@ -6,39 +6,64 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 
+import Flag from "./assets/images/Flag.jpg";
+import History from "./assets/images/history2.png";
+import Bat133 from "./assets/images/battalions/1-33.png";
+import Bat187 from "./assets/images/battalions/1-187.png";
+import Bat2506 from "./assets/images/battalions/2-506.png";
+import Bat3320 from "./assets/images/battalions/3-320th.png";
+import Bat21BEB from "./assets/images/battalions/21BEB.png";
+import Bat626 from "./assets/images/battalions/626BSB.png";
+import Background from "./assets/images/Background.png";
+import Copic from "./assets/images/COPIC.jpeg";
+import BatHHC from "./assets/images/battalions/HHC.png";
+import Sgm from "./assets/images/SGMPIC.jpeg";
+import Torri from "./assets/images/Torri.png";
+import TorriRak from "./assets/images/TORRIRAK.png";
+import Fit1 from "./assets/images/fitness/fitness1.jpg";
+import Fit2 from "./assets/images/fitness/fitness2.jpg";
+import Fit3 from "./assets/images/fitness/fitness3.jpg";
+import Fit4 from "./assets/images/fitness/fitness4.jpg";
+import Fit5 from "./assets/images/fitness/fitness5.jpg";
+
+import ReggaeOne from "./assets/fonts/ReggaeOne-Regular.ttf"
+import TorriCustom from "./assets/icomoon/icomoon.ttf"
+import NavIcons from "./assets/icomoon/navbar-icons/icomoon.ttf"
+import HubIcons from "./assets/icomoon/hub-icons/icomoon.ttf"
+
 const _loadResourcesAsync = () => {
   return Promise.all([
     Asset.loadAsync([
-      require("./assets/images/Flag.jpg"),
-      require("./assets/images/history2.png"),
-      require("./assets/images/battalions/1-33.png"),
-      require("./assets/images/battalions/1-187.png"),
-      require("./assets/images/battalions/2-506.png"),
-      require("./assets/images/battalions/3-320th.png"),
-      require("./assets/images/battalions/21BEB.png"),
-      require("./assets/images/battalions/626BSB.png"),
-      require("./assets/images/Background.png"),
-      require("./assets/images/COPIC.jpeg"),
-      require("./assets/images/battalions/HHC.png"),
-      require("./assets/images/SGMPIC.jpeg"),
-      require("./assets/images/Torri.png"),
-      require("./assets/images/TORRIRAK.png"),
-      require("./assets/images/fitness/fitness1.jpg"),
-      require("./assets/images/fitness/fitness2.jpg"),
-      require("./assets/images/fitness/fitness3.jpg"),
-      require("./assets/images/fitness/fitness4.jpg"),
-      require("./assets/images/fitness/fitness5.jpg"),
+      Flag,
+      History,
+      Bat133,
+      Bat187,
+      Bat2506,
+      Bat3320,
+      Bat21BEB,
+      Bat626,
+      Background,
+      Copic,
+      BatHHC,
+      Sgm,
+      Torri,
+      TorriRak,
+      Fit1,
+      Fit2,
+      Fit3,
+      Fit4,
+      Fit5
     ]),
     Font.loadAsync({
-      "reggae-one": require("./assets/fonts/ReggaeOne-Regular.ttf"),
-      "torri-custom": require("./assets/icomoon/icomoon.ttf"),
-      "navbar-icons": require("./assets/icomoon/navbar-icons/icomoon.ttf"),
-      "hub-icons": require("./assets/icomoon/hub-icons/icomoon.ttf"),
-    }),
+      "reggae-one": ReggaeOne,
+      "torri-custom": TorriCustom,
+      "navbar-icons": NavIcons,
+      "hub-icons": HubIcons
+    })
   ]);
 };
 
-export default function App() {
+export default function App () {
   const [dataLoaded, setDataLoaded] = useState(false);
 
   if (!dataLoaded) {
