495d133c9413bc4b447b9c9ca6a1b5a6730a4809
Mike <mhwilkes@gmail.com>
2021-03-12 16:45:52 -0500

    fixing var name shadowing

--- tabs/NewsTab.jsx
+++ tabs/NewsTab.jsx
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import axios from "axios";
-import NewsCard from "../components/NewsCard";
-import Colors from "../constants/Colors";
-import { ScrollView } from "react-native-gesture-handler";
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import { ScrollView } from 'react-native-gesture-handler';
+import NewsCard from '../components/NewsCard';
+import Colors from '../constants/Colors';
 
 export default function NewsTab() {
-  let [news, setNews] = useState([]);
+  const [news, setNews] = useState([]);
 
   const images = [
     // would { article1: require("../assets/article-data/article1.jpg") } work? would be better than index logic, will try later
-    require("../assets/article-data/article1.jpg"),
-    require("../assets/article-data/article2.jpg"),
-    require("../assets/article-data/article3.jpg"),
+    require('../assets/article-data/article1.jpg'),
+    require('../assets/article-data/article2.jpg'),
+    require('../assets/article-data/article3.jpg'),
   ];
   useEffect(() => {
     const getNews = async () => {
-      let news = await axios.get("http://localhost:1337/news-articles");
-      news = news.data.map((news, index) => {
-        let image = images[index];
-        return { ...news, image_location: image };
+      const request = await axios.get('http://localhost:1337/news-articles');
+      const articles = request.data.map((article, index) => {
+        const image = images[index];
+        return { ...article, image_location: image };
       });
-      setNews(news);
+      setNews(articles);
     };
     getNews();
   }, []);
@@ -31,23 +33,22 @@ export default function NewsTab() {
   return (
     <>
       <ScrollView style={styles.container}>
-        {news.map(({ id, title, image_location, content }) => {
-          //console.log( image )
-          return (
+        {news.map(({
+          id, title, image_location, content,
+        }) =>
+          // console.log( image )
+          (
             <NewsCard
               key={id}
               title={title}
               image={image_location}
-              onPress={() =>
-                navigation.navigate("News Article", {
-                  title: title,
-                  text: content,
-                  image: image_location,
-                })
-              }
+              onPress={() => navigation.navigate('News Article', {
+                title,
+                text: content,
+                image: image_location,
+              })}
             />
-          );
-        })}
+          ))}
       </ScrollView>
     </>
   );
@@ -57,11 +58,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.white,
-    width: "100%",
+    width: '100%',
   },
   view: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
   },
 });
