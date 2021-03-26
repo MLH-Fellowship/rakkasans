bd1d747b3ab465c298f0ef36d90238b559ef8514
ainneo <ainneo@gmail.com>
2021-03-25 22:32:28 -0700

    added DMOR 3BDE Docs

--- tabs/hubScreens/DMOR_HMOR_Screen.jsx
+++ tabs/hubScreens/DMOR_HMOR_Screen.jsx
@@ -1,26 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import WordDocument from '../../components/WordDocument';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Linking,
+  Image
+} from "react-native";
+import { Card, CardItem } from "native-base";
+import { ScrollView } from "react-native-gesture-handler";
+import Dimensions from "../../constants/Dimensions";
 
 export default function DMOR_HMOR_Screen() {
-  const [dmorHmor, setDmorHmor] = useState([]);
+  const [dmor, setDmor] = useState([]);
+
+  
   useEffect(() => {
-    const getDmorHmor = async () => {
-      try {
-        const response = await fetch('http://192.168.1.208:3000/dmorHmor');
-        const json = response.json();
-        const pageJSON = [
-          { content: '187th Infantry Regiment HMoR and DMoR', type: 'Title' },
-          { content: json, type: 'DmorHmorList' },
-        ];
-
-        setDmorHmor(pageJSON);
-      } catch (error) {
-        console.log(error);
-      }
+    const getDmor = async () => {
+      const request = await axios.get("http://localhost:3001/DMOR-HMORS");
+      const response = request.data;
+      setDmor(response);
     };
-
-    getDmorHmor();
+    getDmor();
   }, []);
 
-  return <WordDocument page={dmorHmor} />;
+    const url = { uri: "https://picsum.photos/300/300" };
+    const titleWidth = Dimensions.window.width * 0.27;
+    const contentWidth = Dimensions.window.width * 0.65;
+
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "100%",
+      flexDirection: "column",
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "500",
+    },
+    header: {
+      fontSize: 20,
+      fontWeight: "700",
+      textAlign: "center",
+    },
+    description: {
+      fontSize: 13,
+    },
+    image: {
+      width: "100%",
+      height: "80%",
+    },
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text style={styles.header} />
+        {dmor.map((type, id) => (
+          <View>
+            <Card key={id}>
+              <Image style={styles.image} source={url} />
+              <CardItem>
+                <Text style={styles.title}>{type.title}</Text>
+              </CardItem>
+              <CardItem>
+                <View>
+                  <Text>Document Links:</Text>
+                  <Text
+                    style={{ color: "blue", fontSize: 13 }}
+                    onPress={() => Linking.openURL(`${type.pdf[0].url}`)}
+                  >
+                    - {type.pdf[0].name}
+                  </Text>
+                </View>
+              </CardItem>
+            </Card>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
--- tabs/hubScreens/The38DEHistoryScreen.jsx
+++ tabs/hubScreens/The38DEHistoryScreen.jsx
@@ -1,7 +1,97 @@
-import React from 'react';
-import WordDocument from '../../components/WordDocument';
-import historyData from '../../assets/word_documents/38de_history';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Linking,
+  Image,
+  Body
+} from "react-native";
+import { Card, CardItem } from "native-base";
+import { ScrollView } from "react-native-gesture-handler";
+import Dimensions from "../../constants/Dimensions";
 
 export default function The38DEHistoryScreen() {
-  return <WordDocument page={historyData} />;
+  const [history, setHistory] = useState([]);
+
+
+  console.log(history);
+  useEffect(() => {
+    const getHistory = async () => {
+      const request = await axios.get("http://localhost:3001/3-bde-histories");
+      const response = request.data;
+      setHistory(response);
+    };
+    getHistory();
+  }, []);
+
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "100%",
+      flexDirection: "column",
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "500",
+    },
+    header: {
+      fontSize: 20,
+      fontWeight: "700",
+      textAlign: "center",
+    },
+    description: {
+      fontSize: 13,
+    },
+    image: {
+      width: "100%",
+      height: "80%",
+    },
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text style={styles.header} />
+        {history.map((type, id) => (
+          <View>
+            <Card key={id}>
+              <Image style={styles.image} source={type.image} />
+              <CardItem>
+                <Text style={styles.title}>{type.title}</Text>
+              </CardItem>
+              <CardItem>
+                <View>
+                  <Text>Document Links:</Text>
+                  <Text
+                    style={{ color: "blue", fontSize: 13 }}
+                    onPress={() => Linking.openURL(`${type.pdf[0].url}`)}
+                  >
+                    - {type.pdf[0].name}
+                  </Text>
+                  <Text
+                    style={{ color: "blue", fontSize: 13 }}
+                    onPress={() => Linking.openURL(`${type.pdf[1].url}`)}
+                  >
+                    - {type.pdf[1].name}
+                  </Text>
+                  <Text
+                    style={{ color: "blue", fontSize: 13 }}
+                    onPress={() => Linking.openURL(`${type.pdf[2].url}`)}
+                  >
+                    - {type.pdf[2].name}
+                  </Text>
+                </View>
+              </CardItem>
+            </Card>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
