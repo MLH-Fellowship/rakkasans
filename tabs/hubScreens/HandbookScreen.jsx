24ecc4576ed4b6fe68deddf546d15485025639d9
ainneo <ainneo@gmail.com>
2021-04-02 00:43:47 -0700

    add bluebook pdf

--- tabs/hubScreens/HandbookScreen.jsx
+++ tabs/hubScreens/HandbookScreen.jsx
@@ -1,10 +1,96 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Linking,
+  Image,
+  Body,
+} from "react-native";
+import { Card, CardItem } from "native-base";
+import { ScrollView } from "react-native-gesture-handler";
+import Dimensions from "../../constants/Dimensions";
 
 export default function BattScreen() {
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getHistory = async () => {
+      const request = await axios.get("http://localhost:3001/blue-books");
+      const response = request.data;
+      setBook(response);
+    };
+    getHistory();
+  }, []);
+
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "100%",
+      flexDirection: "column",
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+    },
+    pdf: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "500",
+      textAlign: "center",
+      margin: 6,
+    },
+    description: {
+      fontSize: 14,
+      textAlign: "center",
+      margin: 6,
+    },
+    header: {
+      fontSize: 20,
+      fontWeight: "700",
+      textAlign: "center",
+    },
+    image: {
+      width: "100%",
+      height: "60%",
+    },
+  });
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>HandbookScreen on TAB</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text style={styles.header} />
+        {book.map((type, id) => (
+          <View>
+            <Card key={id}>
+              <CardItem>
+                <Text style={styles.title}>{type.title}</Text>
+              </CardItem>
+              <Image style={styles.image} source={type.image} />
+              <CardItem>
+                <Text style={styles.description}>{type.description}</Text>
+              </CardItem>
+              <CardItem>
+                <View style={styles.pdf}>
+                  <Text>Blue Book Link:</Text>
+                  <Text
+                    style={{ color: "blue", fontSize: 13 }}
+                    onPress={() => Linking.openURL(`${type.pdf[0].url}`)}
+                  >
+                    - {type.pdf[0].name}
+                  </Text>
+                </View>
+              </CardItem>
+            </Card>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+
